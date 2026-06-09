4004fa228712663b96aa8308a6831e43a683059d
ytakaoka <c0a2514087@edu.teu.ac.jp>
2026-06-09 15:58:27 +0900

    練習７，８，９　背景の残存問題解決、FPS修正

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,23 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg2_img = pg.transform.flip(bg_img, True,False)
+    bg3_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")#こうかとん
     kk_img = pg.transform.flip(kk_img, True,False)
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        x=tmr
+        x=tmr%3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg2_img, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
         screen.blit(kk_img, [300, 200])
 
         pg.display.update()
         tmr += 1        
-        clock.tick(1200)
+        clock.tick(200)
 
 
 if __name__ == "__main__":
